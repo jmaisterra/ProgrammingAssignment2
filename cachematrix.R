eeae4ff86104b4701e534eb7bf7350a965dc679e
jmaisterra <jmaisterra@jmsoft.com.mx>
2015-02-21 11:26:04 -0600

    Last version

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,49 @@
-## This Functions can 1) Create a "Matrix" Object and is able to chache it's inverse, 
-## and 2) Computes de Inverse of a "Matrix" returning the chache value if the same "Matrix"
-## was used before for the computation.
-
-## Function to create a "Matrix" Object and compute its Inverse, returning a cahe value of 
-## the inverse if the "Matrix" passed is IDENTICAL to the last one used.
+## This Functions can 
+## 1) makeCacheMatrix: Create a "Matrix" Object and is able to cache the Matrix and it's inverse, 
+## 2) cacheSolve: Computes de Inverse of a "Matrix" returning the chache value if the same "Matrix"
+## was the last one used for the inverse computation.
 
 makeCacheMatrix <- function(x = matrix()) {
-      cmat <- NULL     ## Init Matrix used to calculate its Inverse
-      minv <- NULL     ## Init Inverse of the Matrix
+      cmat <- NULL                                ## Init Matrix used to calculate its Inverse
+      minv <- NULL                                ## Init Inverse of the Matrix
       
-      set <- function(y) {     ## Funtion to set the new Matrix, init x, cmat and minv
-            x <<- y
-            cmat <<- y
-            minv <<- NULL
+      set <- function(y) {     
+            x <<- y                               ## Set the new Matrix, 
+            cmat <<- y                            ## Copy the Matrix to the cache value cmat
+            minv <<- NULL                         ## Init the cache Inverse value minv to NULL
       }
-      get <- function() x                                 ## get the Matrix to compute its Inverse
+      
+      get <- function() x                         ## Get the Matrix to compute its Inverse
+      
       setinverse <- function(inverse, x) {
-            minv <<- inverse                             ## set de Inverse to the cache value
-            cmat <<- x                                   ## set de Computad Matrix de the cache value
+            minv <<- inverse                      ## Save the calculated Inverse to the cache value
+            cmat <<- x                            ## Save the Matrix used in the calculation to the cache value
       }
-      getinverse <- function() minv                       ## get the Inverse valua from the cache variable
-      getlastmatrix <- function() cmat                    ## get the Last Matrix used to compute the inverseInverse valua variable
+      
+      getinverse <- function() minv               ## Get the Inverse valua from the cache variable
+      
+      getlastmatrix <- function() cmat            ## Get the Last Matrix used to compute the Inverse
+      
       list(set = set, get = get,
            setinverse = setinverse,
-           getinverse = getinverse)
+           getinverse = getinverse)               ## Return the list of functions: set, get, setinverse, getinverse
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-      minv <- x$getinverse()
-      cmat <- x$getlastmatrix()
-      if( (!is.null(cmat) && (!is.null(minv)) && identical(camt, x) = TRUE) {
+      cmat <- x$getlastmatrix()                   ## Get the last Matrix used to compute the cache
+      minv <- x$getinverse()                      ## Gets the the last computed Inverse from the cache variable
+      
+      ## If a Matrix was used before AND there is a value in the cache value of the inverse
+      ## AND the new Matrix x is equal the one used before, return the cache value
+      if( (!is.null(cmat)) && (!is.null(minv)) && identical(camt, x) == TRUE) {
             message("getting cached data")
             return(minv)
       }
-      vmatrixdata <- x$get()
-      minv <- solve(vmatrix)
-      x$setinverse(minv, x)
-      minv
+      
+      vmatrixdata <- x$get()                      ## Get the Matrix data
+      minv <- solve(vmatrix)                      ## Calculate it`s Inverse
+      x$setinverse(minv, x)                       ## Save the Inverse result and the Matrix Used in the cache variables
+      minv                                        ## Return the Inverse result
 }
+
